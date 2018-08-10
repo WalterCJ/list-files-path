01c2b314020a0e545feafa67cbf8cf4cb4b245c4
WalterCJ <waltercavalcantejunior@gmail.com>
2018-08-10 09:55:49 -0300

    Add test to not list folders

--- test/index.spec.js
+++ test/index.spec.js
@@ -16,7 +16,12 @@ describe('listFilePath', function () {
         })
         it('Should return a list without hidden files', function () {
             const files = file.listFilePath(dirname)
-            expect(files[0].startsWith('.')).to.be.false 
+            expect(files).to.not.include('.files0')
+        })
+        it('Should return a list without folders', function () {
+            const files = file.listFilePath(dirname)
+            expect(files).to.be.an('array').to.not.include('.test')
+            expect(files).to.be.an('array').to.not.include('test2')
         })
     })
     after(function(done) {
