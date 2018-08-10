b6cca2bdab0358b46b82416d5d12a0f6e4b20e73
WalterCJ <waltercavalcantejunior@gmail.com>
2018-08-10 09:53:19 -0300

    Add folders

--- test/util/handleFolders.js
+++ test/util/handleFolders.js
@@ -7,16 +7,18 @@ const dirnameDestination = path.join(__dirname, 'files_destination')
 
 function createFiles(){ 
     fs.mkdirSync(dirnameOrigin)
+    fs.mkdirSync(path.join(dirnameOrigin, '.test') )
+    fs.mkdirSync(path.join(dirnameOrigin, 'test2') )
     fs.mkdirSync(dirnameDestination)
+
     const fiveDays = 24*60*60*1000*5
     
     for(let i=0; i< 10;i++) {
-        let filePath 
+        let filePath = path.join(dirnameOrigin, `files${i}`)
         if(i==0){
             filePath = path.join(dirnameOrigin, `.files${i}`)
-        }else{
-            filePath = path.join(dirnameOrigin, `files${i}`)
         }
+        
         fs.writeFile(filePath,i, (error) => {
             if(error) throw error;
     
